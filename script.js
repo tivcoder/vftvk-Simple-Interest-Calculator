ae1c5b99b09eee10841fc0064a4f2a948bb441bd
tivcoder <69081163+tivcoder@users.noreply.github.com>
2020-12-01 02:04:45 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+
+
+
+function compute() {
+    
+//setting variables in the function
+var principal = document.getElementById("principal").value;
+var rate = document.getElementById("rate").value;
+var years = document.getElementById("years").value;
+var interest = principal * years * rate / 100;
+
+// Converts 'No of years' into actual year in the future
+var futureyear = parseInt(years) + 2020
+var result = document.getElementById("result");
+
+// Displays text in inner.HTML with breaks and highlights
+result.innerHTML = "If you deposit <span class='highlight'>"+ principal + "</span> ,<br> at an interest rate of <span class='highlight'>" + rate + "</span> <br> You will receive an amount of <span class='highlight'>" + interest + "</span>, <br> in the year <span class='highlight'>" + futureyear +"</span>" 
+
+// Validation shows alert if input is zero or negative number
+var principal = document.getElementById("principal").value;
+  if (principal <= 0) { 
+    alert("Enter a positive number");
+    principal.focus();
+    return false;
+
+  } 
+}
+
+// Displays value of range slider and displays adjacent to "interest rate"
+function setSliderLabel() {
+    var rate = document.getElementById("rate").value;
+     document.getElementById("rate_span").innerHTML = rate + "%";
+ }
+
+ 
+ 
+  
+
+
+ 
+        
